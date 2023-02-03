0061fb6c2347de0ac11160ac4f423e5ca676f40e
Americo Bocchini <albocchini@gmail.com>
2023-02-03 14:11:00 -0300

    Criação das enitidades e interfaces do repositorio

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<Product> GetProductsByIdAsync(int id);
+    Task<Product> CreateAsync(Product product);
+    Task<Product> UpdateAsync(Product product);
+    Task<Product> RemoveAsync(Product product);
+
+    Task<Product> GetProductCategoryAsync(int id);
+}
