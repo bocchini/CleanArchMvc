24a83d37c980e9a912e67314695b7343843fd74f
Americo Bocchini <you@example.com>
2023-02-22 20:32:26 -0300

    Create DTOS and Mapping

---
+++ CleanArchMvc.Application/Mappings/DomainToDtoMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Application.Dtos;
+
+namespace CleanArchMvc.Application.Mappings;
+
+public class DomainToDtoMappingProfile: Profile
+{
+	public DomainToDtoMappingProfile()
+	{
+		CreateMap<Category, CategoryDto>().ReverseMap();
+		CreateMap<Product, ProductDto>().ReverseMap();
+	}
+}
